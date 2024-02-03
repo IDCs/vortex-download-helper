50d750405100ca49ff8e7ba77a0f8dcf78672198
IDCs <idc.dts@gmail.com>
2024-02-03 08:58:06 +0000

    minor stuff

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+import axios from 'axios';
 import { download } from './downloader';
 import { findModByFile, findDownloadIdByFile, walkPath } from './util';
-export { download, findModByFile, findDownloadIdByFile, walkPath };
+import { types } from 'vortex-api';
+
+export { axios, download, findModByFile, findDownloadIdByFile, walkPath };
+
+function init(context: types.IExtensionContext) {
+  return;
+}
+
+export default init;
