4ecb327e1fe5e3466e258b54bbabe6f7b037d0d9
IDCs <idc.dts@gmail.com>
2024-07-04 09:48:45 +0100

    allow ts-loader to compile if downloader is distributed as a module

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,2 +1,10 @@
 let webpack = require("vortex-api/bin/webpack").default;
-module.exports = webpack("index", __dirname, 5);
+module.exports = {
+  ...webpack("index", __dirname, 5),
+  output: {
+    filename: 'downloader.js',
+  },
+  module: {
+    rules: [{ test: /\.ts$/, loader: 'ts-loader', options: { allowTsInNodeModules: true } }],
+  },
+}
